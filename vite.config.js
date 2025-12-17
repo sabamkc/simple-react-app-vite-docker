91dd8779c88b4d7e9c02f86782c63349d7695170
sabamkc <sabarimuthukumarc@gmail.com>
2025-12-17 22:25:21 +0000

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // Set base to the repository name for GitHub Pages
+  base: '/simple-react-app-vite-docker/',
   plugins: [react()],
   server: {
     host: "0.0.0.0",
